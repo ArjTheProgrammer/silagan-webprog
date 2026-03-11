cb02723d3dcc73d5a2f8d0b05646ab3e49365465
Jose Neil Silagan Jr. <neilsilagan@gmail.com>
2026-03-11 13:27:02 +0800

    lab-act1

--- silagan-client/src/App.jsx
+++ silagan-client/src/App.jsx
@@ -7,28 +7,17 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className="App">
+      <div className="App-header">
+        <h1>Welcome to My React App</h1>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Name: Jose Neil Silagan <br />
+          Email: neilsilagan@gmail.com <br />
+          GitHub: ArjTheProgrammer <br />
+          <a href="https://github.com/ArjTheProgrammer/silagan-webprog">Click here to see the repo!</a>
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
